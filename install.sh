5b867a27263993ed19597abefc69b971a0410e6e
root <root@server-xyzq4h.novalocal>
2026-05-01 20:38:05 +0000

    V0.2.0-8: Update installer version

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Obhod VPN One-Line Installer v0.2.0-6
+# Obhod VPN One-Line Installer v0.2.0-8
 # Usage: sh <(wget -qO- "https://raw.githubusercontent.com/goodbrat-lab/obhod-vpn/main/install.sh?$(date +%s)")
 
 set -e
 
-echo "--- Obhod VPN Installer v0.2.0-6 ---"
+echo "--- Obhod VPN Installer v0.2.0-8 ---"
 
-PKG_URL="https://raw.githubusercontent.com/goodbrat-lab/obhod-vpn/main/dist/obhod_0.2.0-6_all.ipk"
+PKG_URL="https://raw.githubusercontent.com/goodbrat-lab/obhod-vpn/main/dist/obhod_0.2.0-8_all.ipk"
 
 # Download and Install
 echo "Downloading Universal Package (Shell Backend + Vue UI)..."
